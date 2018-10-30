854aa514475113d85277f46e93493bfbded7da55
ahb108 <andrew.bevan@gmail.com>
2018-10-30 10:46:24 +0000

    map elements

--- R/mapcomponents.R
+++ R/mapcomponents.R
@@ -79,3 +79,24 @@ gradSymbols <- function(x, mincex, maxcex, valrange=NULL, sqrttrans=FALSE,...){
   y <- mincex + (((x-minx)*maxcex - (x-minx)*mincex) / (maxx-minx))
   return(y)
 }
+
+
+#' Addition of both a north arrow and scalebar to map
+#'
+#' This is a helper function to add both north arrow and scalebar to a map.
+#'
+#' @param x x coordinate for the centre of the added elements (the centre of the text label)
+#' @param y y coordinate for the centre of the added elements (the centre of the text label)
+#' @param scalesize width of the scalebar in map units (this function assumes projcted units but will not throw a warning if unprojected units are used.
+#' @param scalelabel text label for the state width of the scalebar (it requires the user to enter this and is not automatically generated from scalesize)
+#' 
+#' @return a plotted north arrow and scale-bar
+#' @export
+#' 
+mapElements <- function(x, y, scalesize, scalelabel, cex.label=0.75, type.north="simple", lwd.north=0.5, offset.north=height.scale*1.5, r.north=scalesize/5, bkgrd.north="white", fill.north="black", offset.scale=height.scale, height.scale=scalesize/8, border.scale="black", col.scale="white", lwd.scale=0.5){
+    
+    northArrow(x, y+(r.north+offset.north), r=r.north, type=type.north, lwd=lwd.north, fill=fill.north, bkgrd=bkgrd.north)
+    scaleBar(x-(scalesize/2), y-(offset.scale), width=scalesize, height=height.scale, lwd=lwd.scale, col=col.scale, border=border.scale)
+    text(x, y, labels=scalelabel, cex=cex.label)
+
+}
