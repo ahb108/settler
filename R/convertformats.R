46dece530b3a07be56d13f5e297e8bf744bd2cfe
ahb108 <andrew.bevan@gmail.com>
2019-09-28 11:43:39 +0100

    Add NGR resolution

--- R/convertformats.R
+++ R/convertformats.R
@@ -1,8 +1,15 @@
 
+#' @title Resolve a Britsh or Irish National Grid Reference. 
+#' @description Convert a Britsh or Irish National Grid Reference to a coordinate pair.
+#' @param ngr A character string with a British National Grid Reference
+#' @param centre Return the coordinate of the origin of the local square (default) or the centre.
+#' @param grid Input grid system (currently British or Irish)
+#' @return A numeric coordinate pair
+#' @examples
+#' ngrResolve("SJ456782")
 #' @export
 ngrResolve <- function(ngr, centre=FALSE, grid="British"){
   if (!is.na(ngr)){
-      ##Convert UK grid references to full coordinates
       ngr <- gsub(pattern="[[:space:]]+", replacement="", ngr)
       lets <- substr(ngr,1,2)
       if (grid=="British"){
@@ -57,6 +64,65 @@ ngrResolve <- function(ngr, centre=FALSE, grid="British"){
       return(NA)
   }
 }
+
+#' @title Change the resolution of a British National Grid Reference. 
+#' @description Increase of decrease the resolution of a British National Grid Reference. 
+#' @param x A character string with a British National Grid Reference
+#' @param figures The desired precision (an even whole number)
+#' @return A character string with a new British National Grid Reference
+#' @examples
+#' x <- "SJ456782"
+#' ngrResolution(x,4)
+#' ngrResolution(x,8)
+#' ngrResolution(x,7)
+#' @export
+ngrResolution <- function(x, figures){
+
+    if (figures%%1!=0 | figures < 0 | figures %% 2 != 0){
+        stop("The figures argument must be an even, whole number greater than or equal to 0.")
+    }
+    prefix <- substr(x,1,2)
+    suffix <- substr(x,3,nchar(x))
+    if (nchar(suffix) %% 2 == 0){
+        xcomp <- substr(suffix,1,nchar(suffix)/2)
+        ycomp <- substr(suffix,(nchar(suffix)/2)+1,nchar(suffix))
+        if (figures <= nchar(suffix)){
+            newxcomp <- substr(xcomp,1, figures/2)
+            newycomp <- substr(ycomp,1, figures/2)
+            newngr <- paste(prefix, newxcomp, newycomp, sep="")
+        } else {
+            add0s <- (figures - nchar(suffix))/2
+            add0s <- paste0(rep("0",add0s), collapse="")
+            newngr <- paste(prefix, xcomp, add0s, ycomp, add0s, sep="")
+        }
+    } else {
+        stop("Grid reference has an incorrect (odd) number of figures.")
+    }
+    return(newngr)
+}
+
+#' @title Convert a coordinate pair to a different coordinate system 
+#' @description Converts a single coordinate pair to a different coordinate system  
+#' @param coords A vector of length 2 with the coordinates
+#' @param proj1 The CRS of the input coordinates 
+#' @param proj2 The target CRS of the output coordinates
+#' @return A vector of length 2 with the converted coordinate pair.
+#' @examples
+#' bng <- CRS("+init=epsg:27700") # British National Grid
+#' lonlat <- CRS("+init=epsg:4326") # LatLon WGS84
+#' xyTransform(c(345600,378200), bng, lonlat)
+#' @export
+xyTransform <- function(coords, proj1, proj2){
+    if (as.character(class(proj1))!="CRS" | as.character(class(proj2))!="CRS"){
+        stop("One or both of proj1 and proj2 are not correct CRS class definitions.")
+    }
+    tmp <- SpatialPoints(t(as.matrix(coords)), proj4string=proj1)
+    tmp <- spTransform(tmp,proj2)
+    coordinates(tmp)
+    res <- as.numeric(coordinates(tmp))
+    return(res)
+}
+
 
 ##
  
